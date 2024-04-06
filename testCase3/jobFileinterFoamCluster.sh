52102689dc272a81fc11fe8a35454c43b5cfd06d
gujacobi <79080505+gujacobi@users.noreply.github.com>
2024-04-06 23:32:42 +0200

    Add files via upload

---
+++ testCase3/jobFileinterFoamCluster.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+#SBATCH --job-name="ship"
+#SBATCH --time=20:00:00
+#SBATCH --partition=compute
+#SBATCH --ntasks=12
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --account=education-me-courses-mt44025
+
+module load 2022r2
+module load openmpi
+module load openfoam
+
+source /apps/arch/2022r2/software/linux-rhel8-skylake_avx512/gcc-8.5.0/openfoam-2106-6iqyrrzl6jyb3mpzcy4cqohas5dqmgwc/etc/bashrc
+
+srun --cpu-bind=none interFoam  -parallel >run.log
---
+++ testCase3/step2Cluster.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+module load 2022r2
+module load openmpi
+module load openfoam
+
+source /apps/arch/2022r2/software/linux-rhel8-skylake_avx512/gcc-8.5.0/openfoam-2106-6iqyrrzl6jyb3mpzcy4cqohas5dqmgwc/etc/bashrc
+
+
+
+decomposePar
+sbatch jobFilesnappyHexMeshCluster.sh
